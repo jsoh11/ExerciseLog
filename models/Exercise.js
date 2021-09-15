ca6527577b25e7176059259a2a4c428060846888
Eric Duwe <ericduwe@gmail.com>
2021-09-14 21:08:26 -0500

    added models for exercise posts and users, plus index.js for models folder

---
+++ models/Exercise.js
@@ -0,0 +1,55 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Exercise extends Model {}
+
+Exercise.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    activity_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    distance: {
+      type: DataTypes.DECIMAL(10, 1),
+      allowNull: false,
+    },
+    time: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'exercise',
+  }
+);
+
+module.exports = Exercise;
