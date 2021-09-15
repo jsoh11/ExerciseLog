540445627e8805284b972b869efe845dc5164d82
Prany <77269d@gmail.com>
2021-09-15 12:29:35 -0500

    updated index with exerciseRoutes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 
+const exerciseRoutes = require('./exerciseRoutes');
+
 const userRoutes = require('./userRoutes');
+
+router.use('/exercises', exerciseRoutes);
 
 router.use('/users', userRoutes);
 
